f318da5a0aba86ea06fcabefd0dd3a77985e197c
jacwilton <40304015+jacwilton@users.noreply.github.com>
2018-08-01 08:37:57 -0400

    one phone number

--- daily_inspo.py
+++ daily_inspo.py
@@ -4,11 +4,11 @@ from pymongo import MongoClient
 from twilio.rest import Client
 import time
 
+
 # Account SID from twilio.com/console
 account_sid = "ACc4dad3733a3552567bff7a53caaeb429"
 # Auth Token from twilio.com/console
 auth_token  = "d9c34a5c90cdb9d5935c89603c996de6"
-
 
 
 if __name__ == '__main__':
@@ -23,9 +23,17 @@ if __name__ == '__main__':
     post_authors = soup.find('p', class_='author').text
     
     if soup.find('post_authors') is None:
+<<<<<<< HEAD
         fromNumber = '+16314029977'
-        toNumber = '+16318735203, +2019561246'
+        toNumber = '+16318735203'
         message = (post_quotes + "\n" + post_authors)
         client.messages.create(to=toNumber, from_=fromNumber, body=message)
+=======
+        client.messages.create (
+            from_= '+16314029977'
+            to= '+16318735203'
+            body= post_quotes + "\n" + post_authors
+            )
+>>>>>>> 9d4e59aaf50ec4bc5efc0409dcacb35232964aa5
 
 

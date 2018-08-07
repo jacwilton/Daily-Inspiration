110b13a6e457e06a8dd4840cb5bdcda248d35263
jacwilton <40304015+jacwilton@users.noreply.github.com>
2018-08-07 13:52:07 -0400

    using google sheets

--- daily_inspo.py
+++ daily_inspo.py
@@ -2,13 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from twilio.rest import Client
-import time
+from datetime import datetime
+from threading import Timer
+from numpy import *
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find workbook by name and open first sheet
+sheet = client.open('daily_inspo').sheet1
+
+
+x=datetime.today()
+y=x.replace(day=x.day+1, hour=15, minute=57, second=0, microsecond=0)
+delta_t=y-x
+
+secs=delta_t.seconds+1
+
 
 
 # Account SID from twilio.com/console
 account_sid = "ACc4dad3733a3552567bff7a53caaeb429"
 # Auth Token from twilio.com/console
 auth_token  = "d9c34a5c90cdb9d5935c89603c996de6"
+service_sid = "MG5bcd49680626fc4e0d5ee41079ba9b6d"
+
 
 
 if __name__ == '__main__':
@@ -21,13 +45,20 @@ if __name__ == '__main__':
     post_quotes = article.p.text
 
     post_authors = soup.find('p', class_='author').text
-    
+
+def send_text():
     if soup.find('post_authors') is None:
-        client.messages.create (
-            from_= '+16314029977'
-            to= '+16318735203'
-            body= post_quotes + "\n" + post_authors
-            )
+        for num in range (2,4):
+            numbers = sheet.acell('a' + str(num)).value
+            if not numbers:
+                print ("no number")
+        else:
+                client.messages.create (
+                            from_= service_sid,
+                            to= "+" + numbers,
+                            body= post_quotes + "\n" + post_authors
+                                    )
 
-
+t = Timer(secs,send_text)
+t.start()
 
